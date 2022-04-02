1def9509923b5571bd8f193ead2368fa14f04c84
patrykxly <101190597+patrykxly@users.noreply.github.com>
2022-04-02 19:36:58 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,16 @@
 const logButton = document.getElementById('zaloguj');
-
+let checked = false;
 /* checking if entered data is correct */
 logButton.addEventListener('click', (e) => {
     e.preventDefault();
     const loginInput = document.getElementById('loginput').value;
     const passwordInput = document.getElementById('haslo').value;
-    if(loginInput.length <= 8 || passwordInput.length <= 8)
-        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 id = "regError">Podano nieprawidłowy login i/lub hasło</h2>');
-    
+    if((loginInput.length < 8 || passwordInput.length < 8) && checked == false){
+        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 class = "regError" id = "labelsChecking">Podano nieprawidłowy login i/lub hasło</h2>');
+        checked = true;
+    }
+    else if((loginInput.length >= 8 || passwordInput.length >= 8) && checked == true){
+        document.getElementById('logowanie').removeChild(document.getElementById("labelsChecking"));
+        checked = false;
+    }
 })
