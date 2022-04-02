7c114ef38c8bfdd912b52c20d3c7f0a99418f112
patrykxly <101190597+patrykxly@users.noreply.github.com>
2022-04-02 19:33:12 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,22 +1,46 @@
-var registerButton = document.getElementById("registerBtn");
-var inputs = document.querySelectorAll(".registerInput");
-var emailLabel = document.getElementById("emailLabel");
-var passwordLabel = document.getElementById("hasloLabel");
-var repeatedPassLabel = document.getElementById("hasloRepeatLabel");
-var loginLabel = document.getElementById("loginLabel");
-
+const registerButton = document.getElementById("registerBtn");
+const inputs = document.querySelectorAll(".registerInput");
+const emailLabel = document.getElementById("emailLabel");
+const passwordLabel = document.getElementById("hasloLabel");
+const repeatedPassLabel = document.getElementById("hasloRepeatLabel");
+const loginLabel = document.getElementById("loginLabel");
+let emailChecked = false;
+let loginChecked = false;
+let passChecked = false;
+let repeatedPassChecked = false;
 
 registerButton.addEventListener("click", (e) =>{
     e.preventDefault();
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailLabel.value) == 0)
-        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 id = "regError">Podano nieprawidłowy adres e-mail!</h2>');
-    if(loginLabel.length >= 8);
-    else
-        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 id = "regError">Login musi zawierać co najmniej 8 znaków!</h2>');
-    if(passwordLabel.length >= 8);
-    else
-        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 id = "regError">Haslo musi zawierać co najmniej 8 znaków!</h2>');
-    if(passwordLabel != repeatedPassLabel)
-        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 id = "regError">Hasła muszą być takie same!</h2>');
-    
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailLabel.value) == 0 && emailChecked == false){
+        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 class = "regError" id = "mailErr">Podano nieprawidłowy adres e-mail!</h2>');
+        emailChecked = true;
+    }
+    else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailLabel.value) != 0 && emailChecked == true){
+        document.getElementById('logowanie').removeChild(document.getElementById("mailErr"));
+        emailChecked = false;
+    }
+    if(loginLabel.value.length < 8 && loginChecked == false){
+        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 class = "regError" id = "loginErr">Login musi zawierać co najmniej 8 znaków!</h2>');
+        loginChecked = true;
+    }
+    else if(loginLabel.value.length >= 8 && loginChecked == true){
+        document.getElementById('logowanie').removeChild(document.getElementById("loginErr"));
+        loginChecked = false;
+    }
+    if(passwordLabel.value.length < 8 && passChecked == false){
+        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 class = "regError" id = "passErr">Haslo musi zawierać co najmniej 8 znaków!</h2>');
+        passChecked = true;
+    }
+    else if(passwordLabel.value.length >= 8 && passChecked == true){
+        document.getElementById('logowanie').removeChild(document.getElementById("passErr"));
+        passChecked = false;
+    }
+    if(passwordLabel.value != repeatedPassLabel.value && repeatedPassChecked == false){
+        document.getElementById('logowanie').insertAdjacentHTML('beforeend', '<h2 class = "regError" id = "repeatedErr">Hasła muszą być takie same!</h2>');
+        repeatedPassChecked = true;
+    }
+    else if(passwordLabel.value == repeatedPassLabel.value && repeatedPassChecked == true){
+        document.getElementById('logowanie').removeChild(document.getElementById("repeatedErr"));
+        repeatedPassChecked = false;
+    }
 })
