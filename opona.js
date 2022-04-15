fd043997eb5f95278163dbb4791c4c40f334cbb0
patrykxly <101190597+patrykxly@users.noreply.github.com>
2022-04-15 20:36:51 +0200

    Update opona.js

--- opona.js
+++ opona.js
@@ -45,20 +45,34 @@ search.addEventListener("keyup", (e) => {
 
 const addingButtons = document.querySelectorAll(".addToCardBtns");
 class Products{
-    constructor(name,price,src, num){
+    constructor(name,price,src, num, amount, isInStorage = false){
         this.name = name;
         this.price = price;
         this.src = src;
         this.num = num;
+        this.amount = amount;
+        this.isInStorage = isInStorage;
     }
 }
+
 
 let j = addingButtons.length;
 window.localStorage.setItem(0,JSON.stringify(j));
 for(let i = 0; i < j; i++){
     addingButtons[i].addEventListener("click", (e) =>{
-        e.preventDefault();
-        let product = new Products(e.target.parentNode.querySelector("h4").textContent,e.target.parentNode.querySelector(".cena").textContent,e.target.parentNode.querySelector("img").src,i+1,0);
-        window.localStorage.setItem(i+1,JSON.stringify(product));
+        if(localStorage.getItem(i+2) && JSON.parse(localStorage.getItem(i+2)).isInStorage == true){
+            const product = JSON.parse(localStorage.getItem(i+2));
+            product.amount+=1;
+            localStorage.setItem(i+2,JSON.stringify(product));
+            console.log(product);
+        }
+        else{
+            const product = new Products(e.target.parentNode.querySelector("h4").textContent,e.target.parentNode.querySelector(".cena").textContent,e.target.parentNode.querySelector("img").src,i+2,1);
+            product.isInStorage = true;
+            localStorage.setItem(i+2,JSON.stringify(product));
+            console.log(product);
+        }
+        localStorage.setItem(1, JSON.parse(localStorage.getItem(1))+1);
+        window.location.reload();
     })
 }
