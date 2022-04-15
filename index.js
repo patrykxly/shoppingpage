51571bf645d6de86325017748d62c9483e42b8aa
patrykxly <101190597+patrykxly@users.noreply.github.com>
2022-04-15 21:31:57 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const oponyContent = document.getElementById("opony");
+const akcesoriaContent = document.getElementById("akcesoria");
+const olejContent = document.getElementById("olej");
+
+oponyContent.addEventListener("click", (e) =>{
+    window.location.replace("https://patrykxly.github.io/shoppingsite/opona.html");
+})
+
+akcesoriaContent.addEventListener("click", (e) =>{
+    window.location.replace("https://patrykxly.github.io/shoppingsite/akcesoria.html");
+})
+
+olejContent.addEventListener("click", (e) =>{
+    window.location.replace("https://patrykxly.github.io/shoppingsite/olej.html");
+})
